c8622267636e12edd1064979cd0c0b9bb564b428
Admin9705 <user@dunn.cloud>
2019-01-17 06:52:40 -0500

    update

--- pressmain.sh
+++ pressmain.sh
@@ -36,6 +36,9 @@ case $typed in
     2 )
         viewcontainers
         mainbanner ;;
+    4 )
+        tldportion
+        mainbanner ;;
     5 )
         deploycontainers
         mainbanner ;;
@@ -172,6 +175,123 @@ $containerlist
 EOF
 
 read -p '💬 Done Viewing? | Press [ENTER]: ' typed < /dev/tty
+}
+
+tldportion () {
+## To Get List for Rebuilding or TLD
+docker ps --format '{{.Names}}' > /tmp/backup.list
+sed -i -e "/traefik/d" /tmp/backup.list
+sed -i -e "/watchtower/d" /tmp/backup.list
+sed -i -e "/word*/d" /tmp/backup.list
+sed -i -e "/x2go*/d" /tmp/backup.list
+sed -i -e "/plexguide/d" /tmp/backup.list
+sed -i -e "/cloudplow/d" /tmp/backup.list
+sed -i -e "/phlex/d" /tmp/backup.list
+
+rm -rf /tmp/backup.build 1>/dev/null 2>&1
+#### Commenting Out To Let User See
+while read p; do
+  echo -n "$p" >> /tmp/backup.build
+  echo -n " " >> /tmp/backup.build
+done </tmp/backup.list
+running=$(cat /tmp/backup.list)
+
+# If Blank, Exit
+if [ "$running" == "" ]; then
+tee <<-EOF
+
+━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+⛔️ WARNING! - No Apps are Running! Exiting!
+━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+EOF
+sleep 3
+exit
+fi
+
+# Menu Interface
+tee <<-EOF
+
+━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+🚀 Traefik - Provider Interface
+━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+
+EOF
+echo PROGRAMS:
+echo $running
+tee <<-EOF
+
+⚠️  NOTE: App Must Be Actively Running! To quit, type >>> exit
+
+━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+EOF
+
+# Standby
+read -p 'Type an Application Name | Press [ENTER]: ' typed < /dev/tty
+
+if [ "$typed" == "exit" ]; then exit; fi
+
+tcheck=$(echo $running | grep "\<$typed\>")
+if [ "$tcheck" == "" ]; then
+  tee <<-EOF
+
+━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+⛔️ WARNING! - Type an Application Name! Case Senstive! Restarting!
+━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+EOF
+sleep 2
+bash /opt/plexguide/menu/traefik/tld.sh
+exit
+fi
+
+if [ "$typed" == "" ]; then
+tee <<-EOF
+
+━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+⛔️ WARNING! - The TLD Application Name Cannot Be Blank!
+━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+EOF
+sleep 2
+bash /opt/plexguide/menu/traefik/tld.sh
+exit
+else
+tee <<-EOF
+
+━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+✅️  PASS: TLD Application Set
+━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+EOF
+
+# Prevents From Repeating
+cat /var/plexguide/tld.program > /var/plexguide/old.program
+echo "$typed" > /var/plexguide/tld.program
+
+sleep 3
+fi
+
+tee <<-EOF
+
+━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+🍖  NOM NOM - Rebuilding Your Old App & New App Containers!
+━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+EOF
+
+sleep 2
+old=$(cat /var/plexguide/old.program)
+new=$(cat /var/plexguide/tld.program)
+
+if [[ "$old" != "$new" && "$old" != "NOT-SET" ]]; then ansible-playbook /opt/plexguide/containers/$old.yml; fi
+
+ansible-playbook /opt/plexguide/containers/$new.yml
+tee <<-EOF
+
+━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+✅️ TLD Containers are Rebuilt!  Process Complete!
+━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+EOF
+sleep 2
+
+### Go Back to Main
+mainbanner
 }
 
 mainbanner
---
+++ tld.sh
@@ -0,0 +1,119 @@
+#!/bin/bash
+#
+# Title:      PlexGuide (Reference Title File)
+# Author(s):  Admin9705
+# URL:        https://plexguide.com - http://github.plexguide.com
+# GNU:        General Public License v3.0
+################################################################################
+
+## To Get List for Rebuilding or TLD
+docker ps --format '{{.Names}}' > /tmp/backup.list
+sed -i -e "/traefik/d" /tmp/backup.list
+sed -i -e "/watchtower/d" /tmp/backup.list
+sed -i -e "/word*/d" /tmp/backup.list
+sed -i -e "/x2go*/d" /tmp/backup.list
+sed -i -e "/plexguide/d" /tmp/backup.list
+sed -i -e "/cloudplow/d" /tmp/backup.list
+sed -i -e "/phlex/d" /tmp/backup.list
+
+rm -rf /tmp/backup.build 1>/dev/null 2>&1
+#### Commenting Out To Let User See
+while read p; do
+  echo -n "$p" >> /tmp/backup.build
+  echo -n " " >> /tmp/backup.build
+done </tmp/backup.list
+running=$(cat /tmp/backup.list)
+
+# If Blank, Exit
+if [ "$running" == "" ]; then
+tee <<-EOF
+
+━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+⛔️ WARNING! - No Apps are Running! Exiting!
+━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+EOF
+sleep 3
+exit
+fi
+
+# Menu Interface
+tee <<-EOF
+
+━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+🚀 Traefik - Provider Interface
+━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+
+EOF
+echo PROGRAMS:
+echo $running
+tee <<-EOF
+
+⚠️  NOTE: App Must Be Actively Running! To quit, type >>> exit
+
+━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+EOF
+
+# Standby
+read -p 'Type an Application Name | Press [ENTER]: ' typed < /dev/tty
+
+if [ "$typed" == "exit" ]; then exit; fi
+
+tcheck=$(echo $running | grep "\<$typed\>")
+if [ "$tcheck" == "" ]; then
+  tee <<-EOF
+
+━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+⛔️ WARNING! - Type an Application Name! Case Senstive! Restarting!
+━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+EOF
+sleep 3
+bash /opt/plexguide/menu/traefik/tld.sh
+exit
+fi
+
+if [ "$typed" == "" ]; then
+tee <<-EOF
+
+━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+⛔️ WARNING! - The TLD Application Name Cannot Be Blank!
+━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+EOF
+sleep 3
+bash /opt/plexguide/menu/traefik/tld.sh
+exit
+else
+tee <<-EOF
+
+━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+✅️  PASS: TLD Application Set
+━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+EOF
+
+# Prevents From Repeating
+cat /var/plexguide/tld.program > /var/plexguide/old.program
+echo "$typed" > /var/plexguide/tld.program
+
+sleep 3
+fi
+
+tee <<-EOF
+
+━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+🍖  NOM NOM - Rebuilding Your Old App & New App Containers!
+━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+EOF
+
+sleep 4
+old=$(cat /var/plexguide/old.program)
+new=$(cat /var/plexguide/tld.program)
+
+if [[ "$old" != "$new" && "$old" != "NOT-SET" ]]; then ansible-playbook /opt/plexguide/containers/$old.yml; fi
+
+ansible-playbook /opt/plexguide/containers/$new.yml
+tee <<-EOF
+
+━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+✅️ TLD Containers are Rebuilt!  Process Complete!
+━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+EOF
+sleep 4
